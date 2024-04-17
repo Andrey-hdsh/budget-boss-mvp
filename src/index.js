60b8df7d5ae5efc18a92748bb037bd2f13009a87
Andrey-hdsh <hdsh138720@gmail.com>
2024-04-17 17:15:37 +0000

    test_settings

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { GlobalStyle } from 'GlobalStyle';
+import { GlobalStyle } from './GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
