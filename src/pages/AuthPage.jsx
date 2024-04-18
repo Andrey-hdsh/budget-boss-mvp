b4bb64e496f9443303025b9a4f9477f7409a7004
Andrey-hdsh <hdsh138720@gmail.com>
2024-04-18 22:33:07 +0000

    add Icon component

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,5 +1,12 @@
+import { Icon } from 'components/Icon';
+
 function AuthPage() {
-  return <div style={{ color: 'white' }}>AuthPage </div>;
+  return (
+    <>
+      <div style={{ color: 'white' }}>AuthPage </div>
+      <Icon width={81} height={20} iconid={'icon-Group-25'} />
+    </>
+  );
 }
 
 export default AuthPage;
