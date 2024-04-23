966393ed2c1a9675ced1f8216d06d03669dbe3fc
SvitLana1990 <svitlana112@outlook.es>
2024-04-23 17:32:40 +0200

    visible-buttons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,20 @@
+import { GoogleButton } from '../components/Buttons/GoogleButton';
+import { LogInButton } from '../components/Buttons/LogInButton';
+import { RegistrationButton } from '../components/Buttons/RegistrationButton';
+import { InputButton } from '../components/Buttons/InputButton';
+import { ClearButton } from '../components/Buttons/ClearButton';
+
 function HomePage() {
-  return <div style={{ color: 'white' }}>HOME PAGE</div>;
+  return (
+    <>
+      <div style={{ color: 'white' }}>HOME PAGE</div>
+      <GoogleButton />
+      <LogInButton />
+      <RegistrationButton />
+      <InputButton />
+      <ClearButton />
+    </>
+  );
 }
 
 export default HomePage;
