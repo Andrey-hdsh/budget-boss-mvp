1380a825384e79deee7eee0eb4abd622bfb937b9
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2024-04-23 22:01:33 +0200

    fix form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,17 +1,36 @@
-import { MainForm, StyledField, StyledLabel, Btn } from './LoginForm.styled';
+import React from 'react';
+import {
+  FormWrapper,
+  Label,
+  InputEmail,
+  InputPassword,
+  LogInBtn,
+  RegisterBtn,
+} from './LoginForm.styled';
 
-export const LoginForm = () => {
+export const LoginForm = ({ errors, register }) => {
   return (
-    <MainForm autoComplete="off">
-      <StyledLabel>
-        Email
-        <StyledField type="email" name="email" />
-      </StyledLabel>
-      <StyledLabel>
-        Password
-        <StyledField type="password" name="password" />
-      </StyledLabel>
-      <Btn type="submit">Log In</Btn>
-    </MainForm>
+    <div>
+      <FormWrapper>
+        <form>
+          <div>
+            <Label>Email:</Label>
+
+            <InputEmail type="email" placeholder="Email" />
+          </div>
+          <div>
+            <Label>Password:</Label>
+
+            <InputPassword type="password" placeholder="Password" />
+          </div>
+          <LogInBtn name="Log in" type="submit">
+            Log in
+          </LogInBtn>
+          <RegisterBtn name="Registration" type="submit">
+            Registration
+          </RegisterBtn>
+        </form>
+      </FormWrapper>
+    </div>
   );
 };
--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -1,43 +1,161 @@
 import styled from 'styled-components';
 
-export const MainForm = styled.form`
+export const FormWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  max-width: 300px;
-  margin: 0 auto;
-  gap: 24px;
-`;
-export const Btn = styled.button`
-  margin-bottom: 20px;
-  border: none;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 1.5;
-  letter-spacing: 0.04em;
-  cursor: pointer;
-  display: block;
-  min-width: 169px;
-  height: 40px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  color: white;
-  background-color: #7c90f3;
-  margin-left: auto;
-  margin-right: auto;
-  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  /* margin-top: 12px; */
+  justify-content: center;
+  align-items: center;
+  background-repeat: no-repeat;
+  height: 94vh;
+  height: calc(var(--vh, 1vh) * 94);
+  /* overflow:hidden; */
+
+  @media screen and (min-width: 768px) {
+    background-position: top center;
+    padding-top: 294px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    display: flex;
+    justify-content: space-around;
+    gap: 4%;
+
+    margin-top: 100px;
+    // padding-top: 0px;
+    padding: 0 125px;
+    background-image: none;
   }
 `;
 
-export const StyledLabel = styled.label`
-  font-size: 25px;
-  font-weight: 500;
-  text-transform: capitalize;
-  text-shadow: 0 1px 1px #2d2020;
+export const Img = styled.img`
+  display: none;
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+  @media screen and (min-width: 1280px) {
+    display: block;
+  }
 `;
 
-export const StyledField = styled.input`
-  width: 100%;
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+export const Form = styled.form`
+  width: 280px;
+  height: 500px;
+  border-radius: 16px;
+  padding: 32px 20px;
+  background-color: var(--grey47);
+  box-shadow: 0px 2px 14px 0px rgba(132, 132, 132, 0.2) inset;
+
+  @media screen and (min-width: 768px) {
+    width: 392px;
+    height: 556px;
+    padding: 52px;
+    border-radius: 30px;
+    box-sizing: border-box;
+    /* margin-bottom: 120px; */
+  }
+
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const Span = styled.span`
+  color: var(--red);
+`;
+
+export const RequiredP = styled.p`
+  font-size: 10px;
+  font-weight: 400;
+  margin-top: -22px;
+  padding-left: 8px;
+  margin-bottom: 8px;
+  letter-spacing: 0.4px;
+  color: var(--red);
+`;
+export const Label = styled.label`
+  font-size: 12px;
+  font-weight: 400;
+  padding-left: 8px;
+  letter-spacing: 0.48px;
+  color: var(--text-color);
+`;
+
+export const InputEmail = styled.input`
+  min-width: 240px;
+  height: 48px;
+  border: none;
+  margin-top: 8px;
+  margin-bottom: 32px;
+  padding-left: 16px;
+  border-radius: 16px;
+  background-color: white;
+
+  @media screen and (min-width: 768px) {
+    width: 272px;
+  }
+
+  @media screen and (min-width: 1280px) {
+  }
+`;
+export const InputPassword = styled.input`
+  min-width: 240px;
+  height: 48px;
+  border: none;
+  margin-top: 8px;
+  margin-bottom: 32px;
+  padding-left: 16px;
+  border-radius: 16px;
+  background-color: white;
+
+  @media screen and (min-width: 768px) {
+    width: 272px;
+  }
+
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const LogInBtn = styled.button`
+  width: 116px;
+  height: 44px;
+  font-size: 12px;
+  border: none;
+  margin-top: 20px;
+  margin-right: 8px;
+  padding: 15px 38px;
+  border-radius: 16px;
+  letter-spacing: 0.24px;
+  background-color: var(--button);
+  box-shadow: 1px 3px 5px 0px rgba(96, 196, 112, 0.35);
+  color: #fff;
+
+  @media screen and (min-width: 768px) {
+    width: 136px;
+    margin-right: 16px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    margin-top: 10px;
+  }
+`;
+
+export const RegisterBtn = styled.button`
+  width: 116px;
+  height: 44px;
+  font-size: 12px;
+  border: none;
+  margin-top: 20px;
+  padding: 15px 13px;
+  border-radius: 16px;
+  letter-spacing: 0.24px;
+  color: var(--background);
+  background-color: var(--text-color);
+  box-shadow: 0px 4px 7px 0px rgba(0, 0, 0, 0.25);
+
+  @media screen and (min-width: 768px) {
+    width: 136px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    margin-top: 10px;
+  }
 `;
