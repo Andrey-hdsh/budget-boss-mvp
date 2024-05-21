48a8c0e1cd26710984c810c3fccde26d7719e2ba
Valentyna1402 <vriabchun@gmail.com>
2024-05-21 18:27:52 +0300

    mobile markup

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,6 @@
 import { AddTransactionForm } from 'components/AddTransactionForm/AddTransactionForm';
 import { Balance } from 'components/Balance/Balance';
+import { Icon } from 'components/Icon';
 import { ReportsLink } from 'components/ReportsLink/ReportsLink';
 import { SummaryListDashboard } from 'components/SummaryListDashboard/SummaryListDashboard';
 import { TableDashboard } from 'components/TableDashboard/TableDashboard';
@@ -8,15 +9,27 @@ function DashboardPage() {
   return (
     <div>
       <div>
-        <Balance />
+        <button>
+          <Icon iconid={'icon-plus'} width={24} height={24} />
+          <p>ADD TRANSACTION</p>
+        </button>
         <ReportsLink />
+        <Balance />
+        <div>
+          <p>Hello! To get started, enter the current balance of your account!</p>
+          <p>You can't spend money until you have it :)</p>
+        </div>
       </div>
-      <div>
+      {/* <div>
         <AddTransactionForm />
         <div>
           <TableDashboard />
           <SummaryListDashboard />
         </div>
+      </div> */}
+      <div>
+        <button>EXPENSES</button>
+        <button>INCOME</button>
       </div>
     </div>
   );
